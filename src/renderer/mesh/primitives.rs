d49e4d841dc7180ab3b3cd07607cde5bd5814db5
roopekt <roope.keto-tokoi@outlook.com>
2023-06-09 23:49:34 +0300

    3D cube 4D mesh
    3D cube, but made out of tertahedrons instead of triangles

--- src/renderer/mesh/primitives.rs
+++ src/renderer/mesh/primitives.rs
@@ -1,7 +1,8 @@
-use super::{Mesh3D, Vertex3D};
+use super::{Mesh3D, Mesh4D, Vertex3D, Vertex4D};
 use glam::{Mat3, Vec3};
 use crate::game::transform::{AffineTransform3D, Transform3D};
 use crate::errors::assert_equal;
+use std::fmt::Debug as DebugTrait;
 
 //yes, this is a very convoluted way to get a quad, but this is easier to generalize to 4D
 pub fn quad_3D() -> Mesh3D {
@@ -14,8 +15,7 @@ pub fn quad_3D() -> Mesh3D {
         let match_count = bool_array_match_count(&corners[0], corner);
         let is_new_diagonal = match match_count {
             0 => true,
-            1 => false,
-            2 => false,
+            1 | 2 => false,
             _ => panic!("impossible match count {}", match_count)
         };
         if is_new_diagonal {
@@ -29,15 +29,15 @@ pub fn quad_3D() -> Mesh3D {
         let corner_i = &corners[i];
         if !corners_of_diagonal.contains(corner_i) {
             triangle_indeces.push(i);
-            triangle_indeces.push(corners.iter().position(|&corner| corner == [!corner_i[0],  corner_i[1]]).unwrap());
-            triangle_indeces.push(corners.iter().position(|&corner| corner == [ corner_i[0], !corner_i[1]]).unwrap());
+            triangle_indeces.push(index_of([!corner_i[0],  corner_i[1]], &corners));
+            triangle_indeces.push(index_of([ corner_i[0], !corner_i[1]], &corners));
         }
     }
     assert_equal!(triangle_indeces.len(), 2*3);
 
     let normal = [0.0, 0.0, 1.0];
     let corner_signs_to_vertex = |signs: &CornerSigns| -> Vertex3D {
-        let corner_sign_to_number =|sign: bool| {
+        let corner_sign_to_number = |sign: bool| {
             match sign {
                 false => -0.5,
                 true => 0.5
@@ -68,6 +68,72 @@ pub fn quad_3D() -> Mesh3D {
 
 pub fn blit_quad() -> Mesh3D {
     quad_3D().as_transformed(&Transform3D { scale: 2.0 * Vec3::ONE, ..Default::default() }.into())
+}
+
+pub fn cube_4D() -> Mesh4D {
+    type CornerSigns = [bool; 3];
+    let corners: Vec<CornerSigns> = all_bool_arrays();
+    assert_equal!(corners.len(), 8);
+
+    let mut corners_of_central: Vec<CornerSigns> = vec![[false, false, false]];
+    for corner in &corners {
+        let match_count = bool_array_match_count(&corners[0], corner);
+        let is_new_diagonal = match match_count {
+            1 => true,
+            0 | 2 | 3 => false,
+            _ => panic!("impossible match count {}", match_count)
+        };
+        if is_new_diagonal {
+            corners_of_central.push(*corner);
+        }
+    }
+    assert_equal!(corners_of_central.len(), 4);
+
+    let mut tetrahedron_indeces: Vec<usize> = corners_of_central
+        .iter()
+        .map(|corner| index_of(*corner, &corners))
+        .collect();
+    for i in 0..corners.len() {
+        let corner_i = &corners[i];
+        if !corners_of_central.contains(corner_i) {
+            tetrahedron_indeces.push(i);
+            tetrahedron_indeces.push(index_of([!corner_i[0],  corner_i[1],  corner_i[2]], &corners));
+            tetrahedron_indeces.push(index_of([ corner_i[0], !corner_i[1],  corner_i[2]], &corners));
+            tetrahedron_indeces.push(index_of([ corner_i[0],  corner_i[1], !corner_i[2]], &corners));
+        }
+    }
+    assert_equal!(tetrahedron_indeces.len(), 5*4);
+
+    let normal = [0.0, 0.0, 0.0, 1.0];
+    let corner_signs_to_vertex = |signs: &CornerSigns| -> Vertex4D {
+        let corner_sign_to_number = |sign: bool| {
+            match sign {
+                false => -0.5,
+                true => 0.5
+            }
+        };
+
+        Vertex4D {
+            position: [
+                corner_sign_to_number(signs[0]),
+                corner_sign_to_number(signs[1]),
+                corner_sign_to_number(signs[2]),
+                0.0
+            ],
+            normal
+        }
+    };
+
+    Mesh4D {
+        vertices: corners
+            .iter()
+            .map(|&c| corner_signs_to_vertex(&c))
+            .collect(),
+        indeces: tetrahedron_indeces
+            .iter()
+            .map(|&i| i.try_into().unwrap())
+            .collect()
+    }
 }
 
 //gives a cube with width 1 and origo as the center
@@ -117,4 +183,8 @@ fn int_to_bool_array<const COUNT: usize>(int: u32) -> [bool; COUNT] {
         .map(|i| ((int >> i) & 1) == 1)
         .collect();
     vec.try_into().unwrap()
+}
+
+fn index_of<T: PartialEq + DebugTrait>(element: T, vec: &Vec<T>) -> usize {
+    vec.iter().position(|e| *e == element).expect(&format!("Didn't find {:?}", element))
 }

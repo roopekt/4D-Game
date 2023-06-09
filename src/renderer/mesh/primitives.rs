39e95e000a0e5d930106d28e4309df294caa716a
roopekt <roope.keto-tokoi@outlook.com>
2023-06-09 18:46:58 +0300

    new algorithm for cube mesh
    easier to generalize to 4D

--- src/renderer/mesh/primitives.rs
+++ src/renderer/mesh/primitives.rs
@@ -1,5 +1,4 @@
 use super::{Mesh, Vertex};
-use std::f32::consts::PI;
 use glam::{Mat3, Vec3};
 use crate::game::transform::{AffineTransform3D, Transform3D};
 
@@ -22,20 +21,27 @@ pub fn blit_quad() -> Mesh {
     quad().as_transformed(&Transform3D { scale: 2.0 * Vec3::ONE, ..Default::default() }.into())
 }
 
-//gives a cube with width 1 and center as origin
+//gives a cube with width 1 and origo as the center
 pub fn cube() -> Mesh {
-    let rotations: Vec<AffineTransform3D> = vec!{
-        Mat3::from_rotation_y(PI *  0.0).into(),
-        Mat3::from_rotation_y(PI *  0.5).into(),
-        Mat3::from_rotation_y(PI *  1.0).into(),
-        Mat3::from_rotation_y(PI *  1.5).into(),
-        Mat3::from_rotation_x(PI * -0.5).into(),
-        Mat3::from_rotation_x(PI *  0.5).into(),
-    };
+    //construct 6 linear transformations (1 per face) such that the Z basis vector gets swapped for any positive or negative basis vector exactly once
+    let mut orthogonal_transforms: Vec<AffineTransform3D> = Vec::with_capacity(2*3);
+    for i in 0..3 {
+        for sign in [-1.0, 1.0] {
+            let mut matrix = Mat3::IDENTITY;
+
+            const Z_INDEX: usize = 2;
+            let column_Z = matrix.col(Z_INDEX);
+            let column_i = matrix.col(i);
+            *matrix.col_mut(i) = column_Z;
+            *matrix.col_mut(Z_INDEX) = sign * column_i;
+
+            orthogonal_transforms.push(matrix.into());
+        }
+    }
     let translation: AffineTransform3D = Transform3D{ position: Vec3::Z * 0.5, ..Default::default() }.into();
 
-    return rotations
+    return orthogonal_transforms
         .iter()
-        .map(|rotation| quad().as_transformed(&(*rotation * translation)))
+        .map(|orthogonal_transform| quad().as_transformed(&(*orthogonal_transform * translation)))
         .sum();
 }

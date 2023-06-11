0631ffafb173fe065cae7f540d3e4cf4a9e5a57b
roopekt <roope.keto-tokoi@outlook.com>
2023-06-11 12:29:03 +0300

    simpler scene

--- src/game/world.rs
+++ src/game/world.rs
@@ -3,7 +3,6 @@ use crate::global_data::GlobalData;
 use crate::renderer::mesh;
 use std::time::Instant;
 use glam::{Mat3, Vec3, Quat};
-use rand::{rngs::SmallRng, SeedableRng, Rng};
 use std::vec::Vec;
 use super::transform::Transform3D;
 use crate::renderer::renderable_object::RenderableObject3D;
@@ -26,43 +25,27 @@ impl World {
 
 fn get_static_scene_objects(display: &glium::Display) -> Vec<RenderableObject3D<materials::SingleColorMaterial3D>> {
     let mut objects: Vec<RenderableObject3D<materials::SingleColorMaterial3D>> = Vec::new();
-    let mut rng = SmallRng::from_entropy();
 
-    let floor_trs = Transform3D {
-        scale: Vec3::splat(100.0),
-        orientation: Mat3::from_quat(Quat::from_rotation_arc_colinear(Vec3::Z, Vec3::Y)),
-        ..Default::default()
-    };
+    //floor
     objects.push(RenderableObject3D {
-        transform: floor_trs.into(),
+        transform: Transform3D {
+            scale: Vec3::splat(100.0),
+            orientation: Mat3::from_quat(Quat::from_rotation_arc_colinear(Vec3::Z, Vec3::Y)),
+            ..Default::default()
+        }.into(),
         mesh: mesh::primitives::quad_3D().upload_static(display),
         material: materials::SingleColorMaterial3D { albedo_color: Vec3::new(1.0, 1.0, 1.0) }
     });
 
-    for _ in 0..15 {
-        const SPAWN_RADIUS: f32 = 7.0;
-        let position = Vec3::new(random_float(&mut rng) * SPAWN_RADIUS, 0.3, random_float(&mut rng) * SPAWN_RADIUS);
-        let orientation = Mat3::from_quat(Quat::from_xyzw(random_float(&mut rng), random_float(&mut rng), random_float(&mut rng), random_float(&mut rng)).normalize());
-
-        objects.push(RenderableObject3D {
-            transform: Transform3D { position, orientation, ..Default::default() }.into(),
-            mesh: mesh::primitives::cube_3D().upload_static(display),
-            material: materials::SingleColorMaterial3D { albedo_color: random_color(&mut rng) }
-        });
-    }
+    //cube
+    objects.push(RenderableObject3D {
+        transform: Transform3D {
+            position: Vec3::new(0.0, 1.0, 3.0),
+            ..Default::default()
+        }.into(),
+        mesh: mesh::primitives::cube_3D().upload_static(display),
+        material: materials::SingleColorMaterial3D { albedo_color: Vec3::new(1.0, 0.0, 0.0) }
+    });
 
     objects
-}
-
-fn random_float<T: Rng>(rng: &mut T) -> f32
-{
-    rng.gen_range(-1.0..1.0)
-}
-
-fn random_color<T: Rng>(rng: &mut T) -> Vec3 {
-    Vec3::new(
-        rng.gen_range(0.0..1.0),
-        rng.gen_range(0.0..1.0),
-        rng.gen_range(0.0..1.0)
-    )
 }

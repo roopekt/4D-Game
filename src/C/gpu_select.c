7b182513b6cf62390bb2ca9741e64b6659eacc4c
Roope Keto-Tokoi <roope.keto-tokoi@outlook.com>
2023-07-18 23:34:29 +0300

    setup for dev on linux

--- src/C/gpu_select.c
+++ src/C/gpu_select.c
@@ -1,5 +1,7 @@
+#ifdef _WIN32
 #include "Windows.h"
 
 //Windows only, from here: https://stackoverflow.com/a/27881472
 __declspec(dllexport) DWORD NvOptimusEnablement = 1;
 __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+#endif
